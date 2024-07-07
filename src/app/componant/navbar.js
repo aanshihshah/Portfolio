72af1ef7065c72bba73e38b4a3a8c1be5d238d5d
Aanshi Shah <169074087+aanshihshah@users.noreply.github.com>
2024-07-07 23:39:53 +0530

    Update navbar.js

--- src/app/componant/navbar.js
+++ src/app/componant/navbar.js
@@ -211,7 +211,7 @@ const[proj,setProj]=useState(true)
     <img class="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-s-lg" src="/profile.jpg" alt=""/>
     <div class="flex flex-col justify-between p-4 leading-normal">
         <h4 class="mb-2 text-2xl font-bold tracking-tight text-white-900 dark:text-white">Aanshi Shah</h4>
-        <p class="mb-3 font-normal text-gray-50 dark:text-gray-50">I hold a Bachelor's degree (BCA) from GLS University, where I studied various programming languages, including C, C++, Python, Java, Joomla, PHP, and database management system. Currently, I am pursuing a Master's in Computer Applications (MCA) at LJ University, where I have further expanded my programming skills. I have completed multiple projects in PHP and Laravel, including a PHP-based cafe multi-vendor management system named Cafexcel and a Laravel-based book rental system.</p>
+        <p class="mb-3 font-normal text-gray-50 dark:text-gray-50">I hold a Bachelors degree (BCA) from GLS University, where I studied various programming languages, including C, C++, Python, Java, Joomla, PHP, and database management system. Currently, I am pursuing a Masters in Computer Applications (MCA) at LJ University, where I have further expanded my programming skills. I have completed multiple projects in PHP and Laravel, including a PHP-based cafe multi-vendor management system named Cafexcel and a Laravel-based book rental system.</p>
     </div>
 </a>
 
