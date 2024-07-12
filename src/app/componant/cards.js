efa138cd6b83f19f28a7ff1a8283af511dd43b9c
Aanshi Shah <169074087+aanshihshah@users.noreply.github.com>
2024-07-12 11:14:03 +0530

    Update cards.js

--- src/app/componant/cards.js
+++ src/app/componant/cards.js
@@ -4,7 +4,7 @@ import React from 'react';
 export default function Cards({ name, desc, image }) {
   return (
     <>
-      <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+      <div className="max-w-sm bg-gray-800 border border-gray-700 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
         <a href="#">
           <img 
             className="rounded-t-lg object-cover w-full h-48" 
@@ -14,11 +14,11 @@ export default function Cards({ name, desc, image }) {
         </a>
         <div className="p-5">
           <a href="#">
-            <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+            <h5 className="mb-2 text-2xl font-bold tracking-tight text-white dark:text-white">
               {name}
             </h5>
           </a>
-          <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
+          <p className="mb-3 font-normal text-gray-400 dark:text-gray-400">
             {desc}
           </p>
         </div>
